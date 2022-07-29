ca9187a66986ca0c39540df0310f38b092f03942
Anton Tofft <tofft.anton@gmail.com>
2022-07-29 15:03:35 +0700

    added backup of startup applications

--- files/gnome/backup-gnome.sh
+++ files/gnome/backup-gnome.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 mkdir -p ~/Dropbox/Backups/$(hostname)/shell-extensions
-cp -r ~/.local/share/gnome-shell/extensions/* ~/Dropbox/Backups/$(hostname)/shell-extensions
+cp -R ~/.local/share/gnome-shell/extensions/* ~/Dropbox/Backups/$(hostname)/shell-extensions
+cp -R ~/.config/autostart ~/Dropbox/Backups/$(hostname)/
 dconf dump / > ~/Dropbox/Backups/$(hostname)/dconf-settings
