264b236b942ce0aa2fecc06ee540dd63942097f4
Anton Tofft <tofft.anton@gmail.com>
2022-08-05 22:23:21 +0800

    Added backup of remmina connections to backup script

--- files/gnome/backup-gnome.sh
+++ files/gnome/backup-gnome.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
+
+# SHELL EXTENSIONS
 mkdir -p ~/Dropbox/Backups/$(hostname)/shell-extensions
 cp -R ~/.local/share/gnome-shell/extensions/* ~/Dropbox/Backups/$(hostname)/shell-extensions
+
+# REMMINA
+mkdir -p ~/Dropbox/Backups/$(hostname)/remmina
+cp -R ~/.var/app/org.remmina.Remmina/data/remmina/* ~/Dropbox/Backups/$(hostname)/remmina
+
+# STARTUP APPS
 cp -R ~/.config/autostart ~/Dropbox/Backups/$(hostname)/
+
+# DCONF SETTINGS (GNOME)
 dconf dump / > ~/Dropbox/Backups/$(hostname)/dconf-settings
